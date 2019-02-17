ef3bdea295dd186676ac4c66ee3bacbd5c1997bc
Binbin Ye <phantom2501@gmail.com>
2019-02-17 17:13:26 +0900

    Add file to xcode project

--- Sources/bssid/utils.swift
+++ Sources/bssid/utils.swift
@@ -1,7 +1,7 @@
 import Foundation
 import CoreWLAN
 
-func scanNetworks(_ interface: CWInterface, name: String? = nil) throws -> Set<CWNetwork> {
+public func scanNetworks(_ interface: CWInterface, name: String? = nil) throws -> Set<CWNetwork> {
   if #available(macOS 10.13, *) {
     return try interface.scanForNetworks(withName: name, includeHidden: true)
   } else {
