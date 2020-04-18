f9a6deb572d966dc1964b360de6ea56df3489607
Binbin Ye <phantom2501@gmail.com>
2020-04-18 12:02:46 +0900

    Acquire location when macOS version >= 10.15

--- Sources/bssid/main.swift
+++ Sources/bssid/main.swift
@@ -1,5 +1,9 @@
 import Guaka
+import CoreLocation
 
 setupCommands()
-
+let locationManager = CLLocationManager()
+if #available(macOS 10.15, *) {
+    locationManager.requestAlwaysAuthorization()
+}
 rootCommand.execute()
--- Sources/bssid/utils.swift
+++ Sources/bssid/utils.swift
@@ -1,7 +1,13 @@
 import Foundation
 import CoreWLAN
+import CoreLocation
 
 public func scanNetworks(_ interface: CWInterface, name: String? = nil) throws -> Set<CWNetwork> {
+    let locationManager = CLLocationManager()
+    if #available(macOS 10.15, *) {
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+    }
   if #available(macOS 10.13, *) {
     return try interface.scanForNetworks(withName: name, includeHidden: true)
   } else {
