ab5a069a319f2c358ff7e64bb5347fdacc0fc621
Binbin Ye <phantom2501@gmail.com>
2020-06-20 12:16:05 +0900

    Handle ssid in connect command

--- Sources/bssid/connect.swift
+++ Sources/bssid/connect.swift
@@ -3,15 +3,17 @@ import Foundation
 import CoreWLAN
 
 var connectCommand = Command(
-        usage: "connect",
-        configuration: configuration,
-        run: execute
+    usage: "connect",
+    configuration: configuration,
+    run: execute
 )
 
 private func configuration(command: Command) {
     command.add(flags: [
-        // Add your flags here
-    ])
+        Flag(longName: "bssid", type: String.self, description: "BSSID of WiFi access point"),
+        Flag(longName: "ssid", type: String.self, description: "SSID of Wifi access point"),
+        Flag(longName: "channel", type: Int.self, description: "Wifi channel of SSID")
+        ])
     // Other configurations
     command.shortMessage = "connect to a WiFi network with given BSSID"
     command.example = "bssid connect 12:ab:cd:34:ef:56"
@@ -19,12 +21,14 @@ private func configuration(command: Command) {
 
 private func execute(flags: Flags, args: [String]) {
     // Execute code here
+    let requestSSID = flags.getString(name: "ssid") ?? ""
+    let requestBSSID = args.count > 0 ? args[0] : ""
+    let requestChannel = flags.getInt(name: "channel") ?? -1
 
-    if (args.count != 1) {
-        print("require a BSSID")
+    if (requestSSID.isEmpty && requestSSID.isEmpty) {
+        print("require a BSSID or pass a SSID by --ssid")
         return
     }
-    let bssidName = args[0]
 
     let wifiClient = CWWiFiClient()
     guard let interface = wifiClient.interface(), interface.powerOn() else {
@@ -37,15 +41,34 @@ private func execute(flags: Flags, args: [String]) {
     }
 
     for network in networks {
-        if network.bssid ?? "" == bssidName {
-            print("Input WiFi password")
-            let password = String(cString: getpass(""))
-            do {
-                try interface.associate(to: network, password: password)
-            } catch {
-                print(String(format: "Failed connecting to %@, bssid %@"),
+        if !((!requestBSSID.isEmpty && network.bssid == requestBSSID) || (!requestSSID.isEmpty && network.ssid == requestSSID)) {
+            continue
+        } else {
+            if requestChannel > -1 && network.wlanChannel?.channelNumber ?? 0 != requestChannel {
+                continue
+            } else {
+                print("Input WiFi password")
+                let password = String(cString: getpass(""))
+                do {
+                    try interface.associate(to: network, password: password)
+                    return
+                } catch {
+                    print(String(format: "Failed connecting to %@, bssid %@"),
                         network.ssid ?? "", network.bssid ?? "")
+                    return
+                }
             }
         }
+    }
+
+    print("Cannot find specified access point")
+    if !requestBSSID.isEmpty {
+        print(String(format: "request BSSID %@", requestBSSID))
+    }
+    if !requestSSID.isEmpty {
+        print(String(format: "request SSID %@", requestSSID))
+    }
+    if requestChannel > -1 {
+        print(String(format: "request channel %@", requestSSID, String(requestChannel)))
     }
 }
